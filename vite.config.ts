7fc95c0beb328539eda702552fec08fb80041d49
Phuongp2003 <phambuinamhphuong@gmail.com>
2025-03-13 01:30:37 +0700

    add vite-plugin-static-copy to duplicate index.html for multiple routes

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,27 @@ import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import vueDevTools from 'vite-plugin-vue-devtools';
 import tailwindcss from '@tailwindcss/vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
 	base: process.env.NODE_ENV === 'github' ? '/ShopMyPham_vue/' : '/',
-	plugins: [vue(), vueJsx(), vueDevTools(), tailwindcss()],
+	plugins: [
+		vue(),
+		vueJsx(),
+		vueDevTools(),
+		tailwindcss(),
+		viteStaticCopy({
+			targets: [
+				{ src: 'index.html', dest: '' }, // Giữ index.html
+				{ src: 'index.html', dest: 'login' },
+				{ src: 'index.html', dest: 'orders' },
+				{ src: 'index.html', dest: 'review' },
+				{ src: 'index.html', dest: 'about' },
+				{ src: 'index.html', dest: '404' },
+			],
+		}),
+	],
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
@@ -18,7 +34,15 @@ export default defineConfig({
 	build: {
 		rollupOptions: {
 			input: {
-				main: fileURLToPath(new URL('./index.html', import.meta.url)),
+				index: 'index.html', // Trang chính
+				login: 'index.html',
+				orders: 'index.html',
+				review: 'index.html',
+				about: 'index.html',
+				'404': 'index.html',
+			},
+			output: {
+				entryFileNames: '[name].js',
 			},
 		},
 	},
